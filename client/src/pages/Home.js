437bcd9aa31d35fb392870c0505991e3b4be8682
Thomasbrown6 <Thomas.brown1125@gmail.com>
2018-10-10 17:35:11 -0400

    made some updates

--- client/src/pages/Home.js
+++ client/src/pages/Home.js
@@ -1,9 +1,12 @@
 import React from "react";
 import { Row, HomeSection } from "../components";
 import "./Home.css";
+import Favicon from 'react-favicon';
+
 
 const Home = () => (
   <Row>
+    <Favicon url="https://thomasabrown.herokuapp.com/static/media/Profilepic.b20333c2.JPG" />
     <HomeSection />
   </Row>
 );
--- client/src/pages/Projects.js
+++ client/src/pages/Projects.js
@@ -1,9 +1,10 @@
 import React from "react";
 import { Row, ProjectSection } from "../components";
-//import "./Portfolio.css";
+import Favicon from "react-favicon";
 
 const Projects = () => (
   <Row>
+    <Favicon url="https://thomasabrown.herokuapp.com/static/media/Profilepic.b20333c2.JPG" />
     <ProjectSection />
   </Row>
 );
